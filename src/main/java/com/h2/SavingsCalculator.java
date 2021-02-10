85d579e9d6304e777bbe17f0f409540f4bf36af2
AnthonyZhang2 <76865712+AnthonyZhang2@users.noreply.github.com>
2021-02-10 01:16:05 -0800

    Update SavingsCalculator.java

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -1,5 +1,6 @@
 package com.h2;
-
+import java.time.LocalDate;
+import java.time.YearMonth;
 public class SavingsCalculator {
     private float [] credits;
     private float[] debits;
@@ -12,9 +13,23 @@ public class SavingsCalculator {
 
     private float sumOfCredits(){
         float sum = 0.0f;
-            for(int i = 0; i < credits.length; i++) {
-                sum += credits[i];
-            }
+        for(int i = 0; i < credits.length; i++) {
+            sum += credits[i];
+        }
         return sum;
+    }
+    private float sumOfDebits(){
+        float sum = 0.0f;
+        for(int i = 0; i < debits.length; i++){
+            sum += debits[i];
+        }
+        return sum;
+
+    }
+    private static int remainingDaysInMonth(LocalDate date){
+        YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+        return remainingDays;
     }
 }
