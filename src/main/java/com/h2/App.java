836781adce8a00d9f958c0a7b735e6e3e82bd1f5
AnthonyZhang2 <76865712+AnthonyZhang2@users.noreply.github.com>
2021-02-10 00:09:02 -0800

    Completed Module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -17,7 +17,9 @@ public class App
 
     private static int add(int[] numbers) {
         int sum = 0;
-        for(int i = 0; i < )
-        return -1;
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
