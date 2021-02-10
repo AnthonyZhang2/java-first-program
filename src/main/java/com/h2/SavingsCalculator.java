3bab6a502c84ce5c6630bf9cd10293d789d2eb1f
AnthonyZhang2 <76865712+AnthonyZhang2@users.noreply.github.com>
2021-02-10 14:43:35 -0800

    Completed module

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -47,8 +47,8 @@ public class SavingsCalculator {
         for(int i = 0; i < debitsAsString.length; i++){
             debits[i] = Float.parseFloat(debitsAsString[i]);
         }
-        SavingsCalculator calculator = new SavingsCalculator(credits, debits);
-        float netSavings = calculator();
+        final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+        float netSavings = calculator.calculate();
         System.out.println("Net Savings = " + netSavings + ", remaining days in month = " +
                 remainingDaysInMonth(LocalDate.now()));
 
