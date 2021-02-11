5e766e13dfd3382e21f804931a7321d0ea6840b1
AnthonyZhang2 <76865712+AnthonyZhang2@users.noreply.github.com>
2021-02-10 23:31:13 -0800

    Completed module

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,5 @@
 package com.h2;
-
+import java.text.DecimalFormat;
 public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
@@ -27,7 +27,19 @@ public class MortgageCalculator {
         float r = getMonthlyInterestRate();
         int n = getNumberOfPayments();
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
-        
+
+    }
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
+    public static void main(String[] args){
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
     }
 
 }
